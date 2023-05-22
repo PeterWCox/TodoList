416d8d116a46334d9835d0c64ec0c9f2f0e6d8c6
Peter Cox <p.cox@outlook.com>
2023-05-22 04:46:34 +0100

    Clean up main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import ReactDOM from 'react-dom/client'
 import './css/global.css'
 import { TodoApp } from './components/TodoApp'
 import { Provider } from 'react-redux'
-import { Header } from './layouts/header/header'
 import { store } from './redux/store'
+import { Header } from './layouts/header/header'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const element = document.getElementById('root') as HTMLElement
+const root = ReactDOM.createRoot(element)
+
+root.render(
     <Provider store={store}>
         <Header />
         <TodoApp />
